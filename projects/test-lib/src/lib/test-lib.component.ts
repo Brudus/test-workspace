927ee21228de716a59d9e4414f8cd2158cd5604f
Manuel Schulze <freelance@manuelschulze.com>
2020-06-17 08:22:29 +0200

    Initial commit

---
+++ projects/test-lib/src/lib/test-lib.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-test-lib',
+  template: `
+    <p>
+      test-lib works! 😊
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TestLibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
